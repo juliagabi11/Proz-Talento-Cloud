55f7289fc4211e683078e351ad2bda0f839970ea
Julia <116929199+juliagabi11@users.noreply.github.com>
2024-08-14 16:36:02 -0300

    Add files via upload

---
+++ cosméticos.py
@@ -0,0 +1,13 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+lista_produtos[1] = 'rímel'
+lista_produtos[4] = 'cremes hidratantes'
+lista_produtos.pop()
+
+
+print(lista_produtos)
+
+
+lista_produtos.append('batons')
+lista_produtos.append('blushes')
+print(lista_produtos)
