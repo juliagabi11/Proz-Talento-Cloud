0e7fc576ce276f16b60ca62f8ace9a000e1cc9a5
Julia <116929199+juliagabi11@users.noreply.github.com>
2024-08-14 16:27:15 -0300

    Add files via upload

---
+++ musicos.py
@@ -0,0 +1,7 @@
+
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+qtd_elementos = len(lista_musicos)
+print(qtd_elementos)
+print(lista_musicos[2])
+print(lista_musicos[9])
+print(lista_musicos[14])
